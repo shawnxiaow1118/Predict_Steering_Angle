e8c5fd9c47be1299a99b384294a37e6548ebd064
xiaowang <wangxiaonku@gmail.com>
2016-11-12 03:43:59 -0500

    modify for predict

--- src/predict.py
+++ src/predict.py
@@ -14,15 +14,19 @@ import os
 import cv2
 import time
 import pandas as pd
+import time
 
 ## image number to predict one step
 BATCH_SIZE = 1
 
 ## test image path
-test_path = "./test_data/test_list.txt"
+# test_path = "./test_data/test_list.txt"
+## or just read the images folder
 
+path = "./test_data/center/"
+test_file_name = os.listdir(path)
 
-test_file_name = read_data.read_test(test_path)
+##test_file_name = read_data.read_test(dirs)
 
 init = tf.initialize_all_variables()
 sess = tf.InteractiveSession()
@@ -35,11 +39,12 @@ train_flag = tf.placeholder(tf.bool, name='train_flag')
 sess.run(init)
 prediction = pred_steer.inference(x, train_flag, drop_prob, wd)
 saver = tf.train.Saver()
-saver.restore(sess, "./save/my-model-183000")
+saver.restore(sess, "./save/my-model-189000")
 
 predictions = []
 names  = []
 for name in test_file_name:
+	start = time.time()
 	#img = cv2.imread('./test_data/center/'+name)
 	image = read_data.read_image("./test_data/center/"+name)
 	pred = sess.run(prediction, feed_dict = {x: [image], train_flag: False, drop_prob:1.0, wd:0.0})
@@ -47,6 +52,8 @@ for name in test_file_name:
 	#cv2.waitKey(1)
 	#time.sleep(0.5)
 	#print(pred[0][0])
+	duration = time.time()-start
+	print("prediction for image: " + name+ " is " +str(pred)+" time: "+str(duration))
 	names.append(str(name[0:19]))
 	predictions.append(pred[0][0])
 
